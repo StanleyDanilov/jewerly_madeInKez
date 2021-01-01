8309fbce89d250abf17faf7a454ce2c8b4c09a2c
StanleyDanilov <dstanleysupreme@gmail.com>
2021-01-01 14:17:36 +0400

    pc version is done

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+// ========== Products Tabs ==========
 document.querySelectorAll('.products__triggers__item').forEach((item) => {
   item.addEventListener('click', function (e) {
     e.preventDefault()
     const id = e.target.getAttribute('href').replace('#', '')
-
-
 
     document.querySelectorAll('.products__triggers__item').forEach(
       (child) => child.classList.remove('products__triggers__item--active')
@@ -18,3 +17,34 @@ document.querySelectorAll('.products__triggers__item').forEach((item) => {
 })
 
 document.querySelector('.products__triggers__item').click()
+
+
+// ========== Smoothly transition ==========
+
+const productsCatalog = document.getElementById('products-catalog')
+const goToCatalog = document.querySelector('.main-btn')
+
+function handleButtonClick() {
+   productsCatalog.scrollIntoView({
+     behavior: 'smooth'
+    })
+}
+
+goToCatalog.addEventListener('click', handleButtonClick)
+
+
+// different type of smooth
+
+// const productCatalog = document.querySelector()
+
+// for (let anchor of anchors) {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault()
+    
+//     const blockID = anchor.getAttribute('href').substr(1)
+    
+//     document.getElementById(blockID).scrollIntoView({
+//       behavior: 'smooth'
+//     })
+//   })
+// }
